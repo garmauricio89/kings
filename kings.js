6a2db1635681fbd9b060053b6020761d6f875110
mauricio garcia <garmauricio89@gmail.com>
2017-04-19 13:15:25 -0400

    kings

---
+++ kings.js
@@ -0,0 +1,79 @@
+var cards = {
+  ha: 'http://ep.yimg.com/ay/incrediblegifts/ace-of-hearts-cardboard-cutout-life-size-standup-1.jpg',
+  ca: 'https://upload.wikimedia.org/wikipedia/commons/2/2a/Ace_of_clubs.png',
+  sa: 'http://1.bp.blogspot.com/_JamXY96v8tY/S8hwP4JXp9I/AAAAAAAAAAU/imAocs2Sju8/s1600/ace_spades.jpg',
+  da: 'http://ep.yimg.com/ay/incrediblegifts/ace-of-diamonds-cardboard-cutout-life-size-standup-1.jpg',
+  h2: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Playing_card_heart_2.svg/614px-Playing_card_heart_2.svg.png',
+c2: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Playing_card_heart_2.svg/614px-Playing_card_heart_2.svg.png',
+s2: 'http://destiny-cards.co.uk/wp-content/uploads/2015/05/two-of-spades-l.png',
+d2: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Playing_card_diamond_2.svg/480px-Playing_card_diamond_2.svg.png',
+h3: 'https://thumb9.shutterstock.com/display_pic_with_logo/170536/134165792/stock-photo-three-of-hearts-playing-card-isolated-on-white-background-134165792.jpg',
+c3: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Playing_card_spade_3.svg/614px-Playing_card_spade_3.svg.png',
+s3: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Playing_card_club_3.svg/614px-Playing_card_club_3.svg.png',
+d3: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Playing_card_diamond_3.svg/500px-Playing_card_diamond_3.svg.png',
+h4: 'http://st.depositphotos.com/2127699/2238/i/950/depositphotos_22389135-stock-photo-playing-card-four-of-hearts.jpg',
+c4: 'https://openclipart.org/image/800px/svg_to_png/192923/1397958273.png',
+s4: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/4_of_spades.svg/706px-4_of_spades.svg.png',
+d4: 'https://ih1.redbubble.net/image.117912159.1017/flat,800x800,075,f.jpg',
+h5: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Playing_card_heart_5.svg/500px-Playing_card_heart_5.svg.png',
+c5: 'http://www.clker.com/cliparts/6/c/1/0/S/B/5-of-clubs.svg',
+s5: 'http://media.istockphoto.com/photos/playing-card-five-of-spades-picture-id156753926',
+d5: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Playing_card_diamond_5.svg/614px-Playing_card_diamond_5.svg.png',
+h6: 'http://media.istockphoto.com/photos/playing-card-six-of-hearts-picture-id166163018',
+c6: 'https://playingcardcollector.files.wordpress.com/2013/04/icelandair_playing_cards_the_six_of_clubs.jpg',
+s6: 'https://thumb7.shutterstock.com/display_pic_with_logo/2535709/319485479/stock-vector-six-of-spades-card-319485479.jpg',
+d6: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Playing_card_diamond_6.svg/384px-Playing_card_diamond_6.svg.png',
+h7: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Playing_card_heart_7.svg/614px-Playing_card_heart_7.svg.png',
+c7: 'http://opengameart.org/sites/default/files/styles/watermarked/public/7_of_clubs.png',
+s7: 'http://media.istockphoto.com/photos/playing-card-seven-of-spades-picture-id158580454',
+d7: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Playing_card_diamond_7.svg/614px-Playing_card_diamond_7.svg.png',
+h8: 'http://www.clker.com/cliparts/h/Z/I/B/f/x/8-of-hearts.svg',
+c8: 'http://st.depositphotos.com/2127699/2238/i/950/depositphotos_22389029-stock-photo-playing-card-eight-of-clubs.jpg',
+s8: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Playing_card_spade_8.svg/614px-Playing_card_spade_8.svg.png',
+d8: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Playing_card_diamond_8.svg/614px-Playing_card_diamond_8.svg.png',
+h9: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_heart_9.svg/500px-Playing_card_heart_9.svg.png',
+c9: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Playing_card_club_9.svg/614px-Playing_card_club_9.svg.png',
+s9: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/Cards-9-Spade.svg',
+d9: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Playing_card_diamond_9.svg/384px-Playing_card_diamond_9.svg.png',
+h10: 'https://thumb9.shutterstock.com/display_pic_with_logo/170536/134165756/stock-photo-ten-of-hearts-playing-card-isolated-on-white-background-134165756.jpg',
+c10: 'http://spiritualnutrition.org/wp-content/uploads/2011/07/273012_206111359436518_110750432305945_542405_7935589_o.jpg',
+s10: 'http://www.robkeister.com/ccprac/410.png',
+d10: 'http://l7.alamy.com/zooms/f0df80047e3a4bbc9bd766dedefeeb20/ten-of-diamonds-playing-card-ejdpnh.jpg',
+hj: 'http://www.madore.org/~david/images/cards/english/jack-hearts.png',
+cj: 'https://cdn.shopify.com/s/files/1/0011/1492/files/jack-clubs.png?1258321008',
+sj: 'https://allaboutcards.files.wordpress.com/2009/06/jack-spades.png',
+dj: 'https://s-media-cache-ak0.pinimg.com/originals/56/ca/0f/56ca0fc4e3fdaa5f831663a95db07e6e.png',
+hq: 'https://s-media-cache-ak0.pinimg.com/736x/15/e4/3a/15e43a712ac44407d23c437b0a5b43bc.jpg',
+cq: 'http://www.madore.org/~david/images/cards/english/queen-clubs.png',
+sq: 'http://www.madore.org/~david/images/cards/english/queen-spades.png',
+dq: 'http://www.madore.org/~david/images/cards/english/queen-diamonds.png',
+hk: 'http://www.madore.org/~david/images/cards/english/king-hearts.png',
+ck: 'http://www.madore.org/~david/images/cards/english/king-clubs.png',
+sk: 'http://www.madore.org/~david/images/cards/english/king-spades.png',
+dk: 'https://allaboutcards.files.wordpress.com/2009/06/king-diamonds.png',
+
+}
+
+
+
+document.getElementsByTagName('img')[0].onclick=function(){
+
+	var card = Object.keys(cards)[Math.floor( Math.random() * Object.keys(cards).length)]
+	document.getElementsByTagName('img')[0].src=cards[card]
+	delete cards[card]
+
+if (card==="ha"||card==="ca"||card==="sa"||card==="da"){document.getElementsByTagName('h1')[0].innerText = "Waterfall!";}
+else if (card==="h2"||card==="c2"||card==="s2"||card==="d2"){document.getElementsByTagName('h1')[0].innerText = "Pick someone to drink!";}
+else if (card==="h3"||card==="c3"||card==="s3"||card==="d3"){document.getElementsByTagName('h1')[0].innerText = "You drink!";}
+else if (card==="h4"||card==="c4"||card==="s4"||card==="d4"){document.getElementsByTagName('h1')[0].innerText = "Dont be the last to touch the floor!";}
+else if (card==="h5"||card==="c5"||card==="s5"||card==="d5"){document.getElementsByTagName('h1')[0].innerText = "Guys drink!";}
+else if (card==="h6"||card==="c6"||card==="s6"||card==="d6"){document.getElementsByTagName('h1')[0].innerText = "Girls drink!";}
+else if (card==="h7"||card==="c7"||card==="s7"||card==="d7"){document.getElementsByTagName('h1')[0].innerText = "Reach for the ceiling!";}
+else if (card==="h8"||card==="c8"||card==="s8"||card==="d8"){document.getElementsByTagName('h1')[0].innerText = "Pick a drinking buddy!";}
+else if (card==="h9"||card==="c9"||card==="s9"||card==="d9"){document.getElementsByTagName('h1')[0].innerText = "Start a rhyme!";}
+else if (card==="h10"||card==="c10"||card==="s10"||card==="d10"){document.getElementsByTagName('h1')[0].innerText = "Pick a category!";}
+else if (card==="hj"||card==="cj"||card==="sj"||card==="dj"){document.getElementsByTagName('h1')[0].innerText = "Never have I ever";}
+else if (card==="hq"||card==="cq"||card==="sq"||card==="dq"){document.getElementsByTagName('h1')[0].innerText = "Questions";}
+else if (card==="hk"||card==="ck"||card==="sk"||card==="dk"){document.getElementsByTagName('h1')[0].innerText = "Make a rule!";}
+
+}
